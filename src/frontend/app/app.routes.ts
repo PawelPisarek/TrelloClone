d0505b3c084642ff6dbdbaf3e005add789a3ac6b
Paweł Pisarek <pisarekp73@gmail.com>
2016-03-31 15:46:38 +0200

    Wycofanie dzialajacego materiala i jwt z CDN

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -1,15 +1,15 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {Login} from './modules/login/login.component';
-import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from "ng2-material/all";
 @Component({
     selector: 'hello-app',
     template: `
-    <login></login>
+    <login>LOAD LOGIN</login>
     `,
    directives: [Login],
 })
 export class HelloApp {
+    name:string = 'World';
 }
 
-bootstrap(HelloApp,[ MATERIAL_PROVIDERS]);
+bootstrap(HelloApp);
--- src/frontend/app/modules/login/login.component.ts
+++ src/frontend/app/modules/login/login.component.ts
@@ -1,43 +1,9 @@
-import {Component, OnInit} from 'angular2/core';
-import {
-    Control, Validators, ControlGroup, AbstractControl, FormBuilder, FORM_DIRECTIVES,
-    CORE_DIRECTIVES
-} from "angular2/common";
-import {loginService} from "./loginService";
-import {Http, HTTP_PROVIDERS} from "angular2/http";
-function skuValidator(control: Control): { [s: string]: boolean } {
-    if (!control.value.match(/^gonto/)) {
-        return {clientName: true};
-    }
-}
+import {Component} from 'angular2/core';
 @Component({
     selector: 'login',
     templateUrl:'app/modules/login/login.html',
-    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
-    providers: [loginService, HTTP_PROVIDERS]
     // styleUrls:['app/styles/build/styles.css']
 })
-export class Login{
-    myForm: ControlGroup;
-    clientName: AbstractControl;
-    _service:loginService;
-
-
-    constructor(fb:FormBuilder, private _service:loginService) {
-        this.myForm = fb.group({
-            'clientName': ['', Validators.compose([
-                Validators.required, skuValidator])]
-        });
-
-        this.clientName = this.myForm.controls['clientName'];
-
-    }
-
-    onSubmit(value:string):void {
-        this._service.auth(value.clientName);
-        this._service.getQuote();
-
-        console.log('you submitted value: ', value, 'Quote ', this._service.quoteOfTheDay);
-    }
+export class Login {
 
 }
