adef955d4ddc8ab1c5305276d6b0a3f635bf26d4
Paweł Pisarek <pisarekp73@gmail.com>
2016-04-21 10:59:12 +0200

    Serwer do poprawy ale POST działa bo api zwraca odpowiedz failed

--- src/frontend/app/modules/login/login.component.ts
+++ src/frontend/app/modules/login/login.component.ts
@@ -7,10 +7,13 @@ import {
 	MATERIAL_DIRECTIVES
 } from "ng2-material/all";
 import {FORM_DIRECTIVES, Validators, FormBuilder, ControlGroup} from "angular2/common";
+import {LoginService} from "./login.service";
+import {HTTP_PROVIDERS} from "angular2/http";
 @Component({
 	selector: 'login',
 	templateUrl: 'app/modules/login/login.html',
-	directives: [MATERIAL_DIRECTIVES, FORM_DIRECTIVES]
+	directives: [MATERIAL_DIRECTIVES, FORM_DIRECTIVES],
+    providers: [LoginService, HTTP_PROVIDERS]
 })
 export class Login {
 	projectForm: ControlGroup;
@@ -18,15 +21,24 @@ export class Login {
 		email: '',
 		password: ''
 	};
-	constructor(fb: FormBuilder) {
+	constructor(fb: FormBuilder, private _loginService:LoginService) {
 		this.projectForm = fb.group({
 			'password': ['', Validators.required],
 			'email': ['', Validators.compose([
-				MdPatternValidator.inline('^.+@.+\..+$'),
-				Validators.required,
-				Validators.minLength(10),
-				Validators.maxLength(100)
+				// MdPatternValidator.inline('^.+@.+\..+$'),
+				// Validators.required,
+				// Validators.minLength(10),
+				// Validators.maxLength(100)
 			])]
 		});
 	}
+    onSubmit(){
+		this._loginService.auth(this.projectForm.value);
+        // console.log(this.projectForm.value);
+        
+    }
+
+    get value(): string {
+        return JSON.stringify(this.loginForm.value, null, 2);
+    }
 }
