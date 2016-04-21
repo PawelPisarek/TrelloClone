ca25cfc837e4f61cfad1e6d000323b4adbf1d4c5
Lisrec <lisrec94@gmail.com>
2016-04-21 12:56:06 +0200

    Server dziala prawidlowo teraz. /login trzeba podac {'login':'admin', 'pass':'admin'}. Mozna tez uzyc /test ktory zawsze zwraca token.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express(), basePath;
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var bodyParser = require('body-parser');
-
-var url = 'mongodb://PM-user:uidas7be21@51.254.221.201:27017/projectManager';
 
 
 if (process.argv.indexOf('--src') === -1) {
@@ -19,53 +15,17 @@ if (process.argv.indexOf('--src') === -1) {
 
 app.use(express.static(basePath));
 app.use("/node_modules", express.static(__dirname + '/../../node_modules/'));
-
 app.use(bodyParser.json());
 
-app.post('/test', function(req, res) {
-  console.log("Yoooooo");
-  console.log(req.headers);
-  console.log(req.body);
-  console.log(req.body.login);
-  res.status(200).send("yay");
-});
 
-app.post('/login', function(data, res){
-    //console.log(data.body);
-        var auth = data.body;
-        
-        //Polaczenie z baza:
-        MongoClient.connect(url, function(err, db) {
-            assert.equal(null, err);
-            var cursor = db.collection('users').find({
-                "auth.login": auth.login
-            });
-			var success = false;
-            cursor.each(function(err, doc) {
-                assert.equal(err, null);
-					if (doc != null) {
-						//Jezeli login i haslo zgadzaja sie:
-						if (doc.auth[0].pass == auth.pass && doc.auth[0].login == auth.login) {
-							console.log("Success login user: ");
-
-							res.json({
-								token: 'iash12e12edias87uhdiase1e12e'
-							});
-							return;
-						}
-					} else {
-						if(!success) {
-							console.log('Failed login with: ' + auth.login + '/' + auth.pass);
-							res.json({failed: true});
-							return;
-						}
-					}
-            });
-        });
-		res.json({failed: true});
-});
+//Loading modules here:
+require('./users')(app);
+//require('./site')(app);
+//require('./upload')(app);
+//require('./admin')(app);
 
 
+//Starting server listen here:
 var server = app.listen(8081, function () {
 	var port = server.address().port
 	console.log("Example app listening at http://127.0.0.1:%s", port)
---
+++ src/server/users.ts
@@ -0,0 +1,59 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require('body-parser');
+
+var url = 'mongodb://PM-user:uidas7be21@51.254.221.201:27017/projectManager';
+
+module.exports = function(app){
+
+    app.post('/test', function(req, res) {
+      res.json({
+          token: 'iash12e12edias87uhdiase1e12e'
+        });
+      //res.status(200).send("ok");
+    });
+
+    app.post('/login', function(data, res){
+        console.log(":POST login start.");
+            var auth = data.body;
+            console.log("Login: " + auth.login);
+            console.log("Haslo: " + auth.pass);
+
+            MongoClient.connect(url, function(err, db) {
+                assert.equal(null, err);
+                var cursor = db.collection('users').find({
+                    "auth.login": auth.login
+                });
+                var success = false;
+                console.log(":Przed kursorem.");
+                cursor.each(function(err, doc) {
+                    assert.equal(err, null);
+                        if (doc != null) {
+                            console.log(":Cos jest w bazie.");
+                            if (doc.auth[0].pass == auth.pass && doc.auth[0].login == auth.login) {
+                                console.log("Udane logowanie.");
+                                //res.status(200).send("ok");
+                                res.json({
+                                    token: 'iash12e12edias87uhdiase1e12e'
+                                });
+                                success = true;
+                            }
+                        } else {
+                            if(!success) {
+                                console.log(":Nieudane logowanie.");
+                                console.log('Failed login with: ' + auth.login + '/' + auth.pass);
+                                res.json({failed: true});
+                            }
+                        }
+                });
+                console.log(":Poza kursorem.");
+            });
+         console.log(":POST login end.");   
+    });
+
+    app.post('/register', function(data, res){
+            var user = data.body;
+            console.log("POST with registering this data: " + user);        
+    });
+    
+}
