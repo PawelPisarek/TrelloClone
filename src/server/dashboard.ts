c66c11a0074f52895c3320502679702852274b2f
Mateusz Ortee <orteedev@gmail.com>
2016-05-19 01:22:37 +0200

    Dasboard: Dodanie wiecej mockow boardow

--- src/server/dashboard.ts
+++ src/server/dashboard.ts
@@ -4,14 +4,49 @@ module.exports = function (app) {
             boards: [
                 {
                     id: "1",
-                    name: "angular2",
-                    author: "Łysy"
+                    name: "NazwaBoardu1",
+                    author: "Autor1"
                 },
                 {
                     id: "2",
-                    name: "express nigdy nie działa",
-                    author: "Kudłaty"
-                }]
+                    name: "NazwaBoardu2",
+                    author: "Autor2"
+                },
+                {
+                    id: "3",
+                    name: "NazwaBoardu3",
+                    author: "Autor1"
+                },
+                {
+                    id: "4",
+                    name: "NazwaBoardu4",
+                    author: "Autor4"
+                },
+                {
+                    id: "5",
+                    name: "NazwaBoardu5",
+                    author: "Autor5"
+                },
+                {
+                    id: "6",
+                    name: "NazwaBoardu6",
+                    author: "Autor6"
+                },
+                {
+                    id: "7",
+                    name: "NazwaBoardu7",
+                    author: "Autor7"
+                },
+                {
+                    id: "8",
+                    name: "NazwaBoardu8",
+                    author: "Autor8"
+                },
+                {
+                    id: "9",
+                    name: "NazwaBoardu9",
+                    author: "Autor9"
+                },]
         });
     });
 
