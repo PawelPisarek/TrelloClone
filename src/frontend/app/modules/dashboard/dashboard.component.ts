ba44637056f640503e7f77b0bf77c674e8d52ba4
Mateusz Ortee <orteedev@gmail.com>
2016-05-18 23:07:09 +0200

    Dashboard: Wczytanie JSONA do templatki

--- src/frontend/app/modules/dashboard/dashboard.component.ts
+++ src/frontend/app/modules/dashboard/dashboard.component.ts
@@ -3,10 +3,15 @@ import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 import {BoardComponent} from "../board/board.component";
+import {BoardService} from "../board/board.service";
+import {Board} from "../board/board.model";
+import {Router} from "angular2/router";
+
+
 @Component({
     selector: 'dashboard',
     templateUrl: 'app/modules/dashboard/dashboard.html',
-    providers: [ HTTP_PROVIDERS],
+    providers: [BoardService,HTTP_PROVIDERS],
     directives: [MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES]
 })
 @RouteConfig([
@@ -14,17 +19,28 @@ import {BoardComponent} from "../board/board.component";
 
 ])
 export class DashboardComponent {
-  list: Object[] = [];
-	constructor(){
-    
-      this.addItem("testid","testname","testautor");
-      this.addItem("testid2","testname2","testautor2");
-      this.addItem("testid3","testname3","testautor3");
-      this.addItem("testid4","testname4","testautor4");
-      this.addItem("testid5","testname5","testautor5");
-      this.addItem("testid6","testname6","testautor6");
-      this.addItem("testid7","testname7","testautor7");
+  list: Object[]=[];
+ _boards:Array<Board>;
+  constructor(private _service:BoardService, private  _router:Router){
 	}
+
+
+  ngOnInit() {
+      this._service.getBoards().subscribe(data=> {
+          this._boards = data;
+          console.log(data[0].id);
+          for(var index in data)
+          {
+            console.log(data[index].name);
+            this.addItem(data[index].id,data[index].name,data[index].author);
+          }
+      });
+
+  }
+
+  onSelect(board:Board) {
+      this._router.navigate(['BoardDetail', {id: board.id}]);
+  }
 
   addItem(_id: string,_name: string,_author: string)
   {
