0a938d7c8852b0b0a6eb99e9496520c80cc7230b
Mateusz Ortee <orteedev@gmail.com>
2016-05-18 15:41:53 +0200

    praca nad wyswietlaniem listy boardow wyslanych przez serwer

--- src/frontend/app/modules/dashboard/dashboard.component.ts
+++ src/frontend/app/modules/dashboard/dashboard.component.ts
@@ -14,9 +14,25 @@ import {BoardComponent} from "../board/board.component";
 
 ])
 export class DashboardComponent {
-
+  list: Object[] = [];
 	constructor(){
 
+      this.addItem("testid","testname","testautor");
+      this.addItem("testid2","testname2","testautor2");
+      this.addItem("testid3","testname3","testautor3");
+      this.addItem("testid4","testname4","testautor4");
+      this.addItem("testid5","testname5","testautor5");
+      this.addItem("testid6","testname6","testautor6");
+      this.addItem("testid7","testname7","testautor7");
 	}
 
+  addItem(_id: string,_name: string,_author: string)
+  {
+      this.list.splice(0,0,
+      {
+        'id': _id,
+        'name': _name,
+        'author': _author
+      });
+  }
 }
