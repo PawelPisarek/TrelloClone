d3654585757686d32296910e66f3d8924754675c
nans0108 <rosicki.1995@gmail.com>
2016-05-22 03:48:21 +0200

    Polaczenie rejestracji z baza mongodb

--- src/frontend/app/modules/registration/registration.service.ts
+++ src/frontend/app/modules/registration/registration.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "angular2/core";
 import {Http, Response, Headers} from "angular2/http";
+import { Router, RouterLink } from 'angular2/router';
 
 export class UserForm {
     constructor(public email:string, public password:string) {
@@ -9,16 +10,28 @@ export class UserForm {
 @Injectable()
 export class UsersService {
 
-    constructor(private http:Http) {
+    constructor(public router: Router, public http: Http) {
     }
-    
-    register(value:UserForm) {
-        const body = `{"email":"${value.email}","password":"${value.password}"}`; // można użyć JSON.stringify(value) tez dziala
-        const header = new Headers();
-        header.append("Content-Type", "application/json");
 
-        return this.http.post('http://localhost:8081/register', body, {headers: header})
-            .map(res => (<Response>res).statusText);
+    register(value: UserForm) {
+        var creds = `{
+            "login": "${value.email}",
+            "pass": "${value.password}"
+        }`;
+        var header = new Headers();
+        header.append('Content-Type', 'application/json');
+        
+        this.http.post('http://localhost:8081/register', creds,
+            {
+                headers: header
+            })
+            .map(res => (<Response>res).json())
+            .subscribe(
+            err => this.logError(err)
+            );
+    }
 
+    private logError(err): any {
+        console.log("wrong" + err);
     }
 }
--- src/server/users.ts
+++ src/server/users.ts
@@ -13,48 +13,43 @@ module.exports = function(app){
       //res.status(200).send("ok");
     });
 
-    app.post('/login', function(data, res){
+    app.post('/login', function(data, res) {
         console.log(":POST login start.");
-            var auth = data.body;
-            console.log("Login: " + auth.login);
-            console.log("Haslo: " + auth.pass);
+        var auth = data.body;
+        console.log("Login: " + auth.login);
+        console.log("Haslo: " + auth.pass);
 
-            MongoClient.connect(url, function(err, db) {
-                assert.equal(null, err);
-                var cursor = db.collection('users').find({
-                    "auth.login": auth.login
-                });
-                var success = false;
-                console.log(":Przed kursorem.");
-                cursor.each(function(err, doc) {
-                    assert.equal(err, null);
-                        if (doc != null) {
-                            console.log(":Cos jest w bazie.");
-                            if (doc.auth[0].pass == auth.pass && doc.auth[0].login == auth.login) {
-                                console.log("Udane logowanie.");
-                                //res.status(200).send("ok");
-                                res.json({
-                                    token: 'iash12e12edias87uhdiase1e12e'
-                                });
-                                success = true;
-                            }
-                        } else {
-                            if(!success) {
-                                console.log(":Nieudane logowanie.");
-                                console.log('Failed login with: ' + auth.login + '/' + auth.pass);
-                                res.json({failed: true});
-                            }
-                        }
-                });
-                console.log(":Poza kursorem.");
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            var cursor = db.collection('users').find({
+                "login": auth.login
             });
-         console.log(":POST login end.");
+            var success = false;
+            console.log(":Przed kursorem.");
+            cursor.each(function(err, doc) {
+                assert.equal(err, null);
+                if (doc != null) {
+                    console.log(":Cos jest w bazie.");
+                    if (doc.pass == auth.pass && doc.login == auth.login) {
+                        console.log("Udane logowanie.");
+                        //res.status(200).send("ok");
+                        res.json({
+                            token: 'iash12e12edias87uhdiase1e12e'
+                        });
+                        success = true;
+                    }
+                } else {
+                    if (!success) {
+                        console.log(":Nieudane logowanie.");
+                        console.log('Failed login with: ' + auth.login + '/' + auth.pass);
+                        res.json({ failed: true });
+                    }
+                }
+            });
+            console.log(":Poza kursorem.");
+        });
+        console.log(":POST login end.");
     });
-
-    // app.post('/register', function(data, res){
-    //         var user = data.body;
-    //         console.log("POST with registering this data: " + user);
-    // });
 
 
     app.get('/api/user', (data, res)=> {
@@ -65,7 +60,43 @@ module.exports = function(app){
     });
 
     app.post('/register', function(data, res) {
-        res.status(200).send({test:"udalo sie"});
+
+        var auth = data.body;
+        console.log("Login: " + auth.login);
+        console.log("Haslo: " + auth.pass);
+        
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            
+            var cursor = db.collection('users').find({
+                "login": auth.login
+            });
+            var success = false;
+            cursor.each(function(err, doc) {
+                assert.equal(err, null);
+                if (doc != null) {
+                    if (doc.login == auth.login ) {
+                        console.log("Uztkownik o takim  loginie juz istnieje");
+                        console.log('Failed sign up with: ' + auth.login);
+                    }
+                    success = true;
+                } else {
+                    if (!success) {
+                        console.log("Mozna stworzyc nowego uzytkownika");
+                        db.collection('users').insert({ "login": auth.login, "pass": auth.pass });
+                        console.log("DODANO UZYTKOWNIKA: login:" + auth.login + " pass:" +auth.pass);
+                    }
+                }      
+                                   
+            });
+
+            //wyswietlanie wszystkich uzytkownikow z bazy
+            /*db.collection('users').find().toArray(function(err, items) {
+                console.log(items);
+            })*/
+           
+        });
+        console.log(":POST signup end.");
     });
 
 }
